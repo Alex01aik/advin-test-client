6b2d1e22da5821db13cc3920b5ef3916665862ac
a.petrov <a.petrov@kuma.family>
2023-04-16 18:40:47 +0200

    delete unnecessary

--- src/components/RegisterForm/validationSchema.ts
+++ src/components/RegisterForm/validationSchema.ts
@@ -1,6 +1,18 @@
 import * as yup from 'yup';
 
+export const passwordValidation = yup
+  .string()
+  .required('is_required')
+  .max(40, 'password_is_long')
+  .min(8, 'password_is_short')
+  .test('low complexity', 'password_low_complexity', (text) => {
+    const numberCase: boolean = /^(?=.*[0-9]).+$/.test(text ?? '');
+    const letterCase: boolean = /^(?=.*[a-z]).+$/.test(text ?? '') && /^(?=.*[A-Z]).+$/.test(text ?? '');
+    const specialSymbolCase: boolean = /^(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).+$/.test(text ?? '');
+    return (letterCase && specialSymbolCase) || (letterCase && numberCase) || (numberCase && specialSymbolCase);
+  });
+
 export const schema = yup.object().shape({
   email: yup.string().email().required(),
-  password: yup.string().min(8).required(),
+  password: passwordValidation,
 });
