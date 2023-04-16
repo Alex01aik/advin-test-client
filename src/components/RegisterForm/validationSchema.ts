3ce49932792a59ec35254b1fa59b8e7e4cec5ea6
a.petrov <a.petrov@kuma.family>
2023-04-16 18:42:04 +0200

    regex

--- src/components/RegisterForm/validationSchema.ts
+++ src/components/RegisterForm/validationSchema.ts
@@ -8,6 +8,7 @@ export const passwordValidation = yup
   .test('low complexity', 'password_low_complexity', (text) => {
     const numberCase: boolean = /^(?=.*[0-9]).+$/.test(text ?? '');
     const letterCase: boolean = /^(?=.*[a-z]).+$/.test(text ?? '') && /^(?=.*[A-Z]).+$/.test(text ?? '');
+    // eslint-disable-next-line no-useless-escape
     const specialSymbolCase: boolean = /^(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).+$/.test(text ?? '');
     return (letterCase && specialSymbolCase) || (letterCase && numberCase) || (numberCase && specialSymbolCase);
   });
