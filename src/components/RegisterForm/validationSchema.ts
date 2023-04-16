8cfa6c30bc2acc9f42368b9bc231beb05fbc0cca
a.petrov <a.petrov@kuma.family>
2023-04-16 18:50:07 +0200

    regex2

--- src/components/RegisterForm/validationSchema.ts
+++ src/components/RegisterForm/validationSchema.ts
@@ -2,16 +2,20 @@ import * as yup from 'yup';
 
 export const passwordValidation = yup
   .string()
-  .required('is_required')
-  .max(40, 'password_is_long')
-  .min(8, 'password_is_short')
-  .test('low complexity', 'password_low_complexity', (text) => {
-    const numberCase: boolean = /^(?=.*[0-9]).+$/.test(text ?? '');
-    const letterCase: boolean = /^(?=.*[a-z]).+$/.test(text ?? '') && /^(?=.*[A-Z]).+$/.test(text ?? '');
-    // eslint-disable-next-line no-useless-escape
-    const specialSymbolCase: boolean = /^(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).+$/.test(text ?? '');
-    return (letterCase && specialSymbolCase) || (letterCase && numberCase) || (numberCase && specialSymbolCase);
-  });
+  .required()
+  .max(40, 'password is too long')
+  .min(8, 'password is too short')
+  .test(
+    'low complexity',
+    'the password must meet at least two conditions: with one number, with upper and lower registy symbol, with special symbol',
+    (text) => {
+      const numberCase: boolean = /^(?=.*[0-9]).+$/.test(text ?? '');
+      const letterCase: boolean = /^(?=.*[a-z]).+$/.test(text ?? '') && /^(?=.*[A-Z]).+$/.test(text ?? '');
+      // eslint-disable-next-line no-useless-escape
+      const specialSymbolCase: boolean = /^(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).+$/.test(text ?? '');
+      return (letterCase && specialSymbolCase) || (letterCase && numberCase) || (numberCase && specialSymbolCase);
+    }
+  );
 
 export const schema = yup.object().shape({
   email: yup.string().email().required(),
